43fb49ee843dae264152b8b7e1bda313d90705b7
Andy HA <adizanni@msn.com>
2024-02-19 01:41:34 +0100

    redesign Dockerfile

--- sh3d-ha/rootfs/usr/local/bin/run.sh
+++ sh3d-ha/rootfs/usr/local/bin/run.sh
@@ -1,9 +1,6 @@
-#!/bin/sh
-
 export HOMES_LOCATION=/share/sh3d-ha/$(jq -r '.model_folder' /data/options.json)
 export EXPORT_LOCATION=/homeassistant/www/sh3d-ha/$(jq -r '.export_to_HA_folder' /data/options.json)
-
 mkdir -p $HOMES_LOCATION
 mkdir -p $EXPORT_LOCATION
-
 catalina.sh run
+
--- sh3d-ha/rootfs/usr/local/tomcat/bin/setenv.sh
+++ sh3d-ha/rootfs/usr/local/tomcat/bin/setenv.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/env bash
-
 export CATALINA_OPTS="$CATALINA_OPTS --add-opens=java.desktop/javax.swing.undo=ALL-UNNAMED"
